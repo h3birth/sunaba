a6acd834586326acd1ffb2078edfba7d6d6d6f86
kako351 <h3.birth@gmail.com>
2019-05-13 01:39:20 +0900

    add MainController

--- android/pagingepoxysample/src/main/java/com/example/pagingepoxysample/MainActivity.kt
+++ android/pagingepoxysample/src/main/java/com/example/pagingepoxysample/MainActivity.kt
@@ -3,15 +3,28 @@ package com.example.pagingepoxysample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import com.airbnb.epoxy.DataBindingEpoxyModel
+import com.example.pagingepoxysample.model.Item
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), MainController.ClickLister {
     private lateinit var viewModel: MainViewModel
+    private val controller by lazy { MainController(this, viewModel) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+
     }
+
+    override fun onItemClickListener(item: Item) {
+
+    }
+
+    override fun onBindLoading(view: DataBindingEpoxyModel.DataBindingHolder) {
+        view.dataBinding.lifecycleOwner = this
+    }
+
 }
